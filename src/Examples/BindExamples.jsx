b93cb93fb774aa0387c81ca40bc24d08344a789a
Ersel Aker <erselaker@gmail.com>
2019-04-10 09:26:42 +0100

    Update BindExamples.jsx

--- src/Examples/BindExamples.jsx
+++ src/Examples/BindExamples.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const DATA_URL = 'https://gist.githkahskjldhfkaubusercontent.com/IkraP/b44da4035619931858183d832497fe6f/raw/70ad77f86583a8940c07fda1a063e23898816ff7/data.json';
+const DATA_URL = 'https://gist.githubusercontent.com/IkraP/b44da4035619931858183d832497fe6f/raw/70ad77f86583a8940c07fda1a063e23898816ff7/data.json';
 
 
 class BindExamples extends React.Component {
